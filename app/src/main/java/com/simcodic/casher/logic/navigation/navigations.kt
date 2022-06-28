826872ad8708b413bbcb21f4bbe0595e09abaa51
Rafal Halasa <rafal.halasa@gmail.com>
2022-06-28 11:50:37 +0200

    fix rotation bug

--- app/src/main/java/com/simcodic/casher/logic/navigation/navigations.kt
+++ app/src/main/java/com/simcodic/casher/logic/navigation/navigations.kt
@@ -1,25 +1,21 @@
 package com.simcodic.casher.logic.navigation
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.simcodic.casher.R
 
 fun AppCompatActivity.setNavigation() {
-    NavigationState.state.observeForever {
-        try {
+    NavigationState.state.observe(this) {
+        it?.let {
             findNavController(R.id.nav_host_fragment).apply {
                 if (checkIfNewDestination(this, it)) {
                     navigate(
                         it.resId, it.bundle
                     )
+                    NavigationState.state.value = null
                 }
             }
-        } catch (illegalStateException: IllegalStateException) {
-            println("IllegalStateException")
-            //TODO something wrong with this checkIfNewDestination when screen is rotating
         }
     }
 }
